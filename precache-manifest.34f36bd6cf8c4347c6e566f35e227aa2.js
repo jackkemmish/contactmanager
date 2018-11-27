7917b4043624373c6679338d4827e78ceeda5703
Jack Kemmish <jkemmish@jkemmish-mbp.local>
2018-11-27 16:36:58 +0000

    Updates

---
+++ precache-manifest.34f36bd6cf8c4347c6e566f35e227aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b9f2b76e141de1a10595",
+    "url": "/contactmanager/static/js/main.b9f2b76e.chunk.js"
+  },
+  {
+    "revision": "57cafbd09e60422168ad",
+    "url": "/contactmanager/static/js/1.57cafbd0.chunk.js"
+  },
+  {
+    "revision": "b9f2b76e141de1a10595",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "57cafbd09e60422168ad",
+    "url": "/contactmanager/static/css/1.563fadfb.chunk.css"
+  },
+  {
+    "revision": "6cfdd3f3ee7c8faf90555357bac6bdb9",
+    "url": "/contactmanager/index.html"
+  }
+];
